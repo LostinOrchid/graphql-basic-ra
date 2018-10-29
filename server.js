4f4c3b9360f47f48e9cef4db43a510ce3b98b080
Virnon Go2designlab <virnont.go2designlab@gmail.com>
2018-10-29 09:20:41 +0800

    mutation with arguments

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ import gql from 'graphql-tag';
 // Optional ra ang gql, para syntax completion/hightlight sa editor.
 const typeDefs = gql`
   type Query {
-    # Comment nimo ani nga query
-    hello(name: String): String!
+      _empty: String
   }
   # Mutations kay kung naa kay data sa server nga ganahan nimo ma add/change like,
   # create/update ug user.
@@ -14,21 +13,18 @@ const typeDefs = gql`
     # adto sa playground then e paste ni
     # {
     #     mutation {
-    #         getRandomInt
+    #         getRandomInt(min: 10, max: 20)
     #     }
     # }
-    getRandomInt: Int!
+    getRandomIntWithinRange(min: Int!, max: Int!): Int!
   }
 `
-// If mana ka dri checkout didto sa mutation-with-args nga branch:
-// git checkout mutation-with-args
+// If mana ka dri checkout didto sa custom-type nga branch:
+// git checkout custom-type
 
 const resolvers = {
-  Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`,
-  },
   Mutation: {
-    getRandomInt:(_, args) => parseInt(Math.random(), 10),
+    getRandomIntWithinRange: (_, { min, max }) => parseInt(Math.floor((Math.random() * max) + min)),
   }
 }
 
