223af60cc6fc12f0ba45af7367ddf4dc95da107a
Virnon Go2designlab <virnont.go2designlab@gmail.com>
2018-10-29 09:14:47 +0800

    Mutation basic

--- server.js
+++ server.js
@@ -4,17 +4,28 @@ import gql from 'graphql-tag';
 // Optional ra ang gql, para syntax completion/hightlight sa editor.
 const typeDefs = gql`
   type Query {
-    # Comment nimo ani nga query
-    hello(name: String): String!
+      _empty: String
+  }
+  # Mutations kay kung naa kay data sa server nga ganahan nimo ma add/change like,
+  # create/update ug user.
+  type Mutation {
+    # Mutation nga walay arguments
+    # adto sa playground then e paste ni
+    # {
+    #     mutation {
+    #         getRandomInt
+    #     }
+    # }
+    getRandomInt: Int!
   }
 `
-// If mana ka dri checkout didto sa mutation nga branch:
-// git checkout mutation
+// If mana ka dri checkout didto sa mutation-with-args nga branch:
+// git checkout mutation-with-args
 
 const resolvers = {
-  Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`,
-  },
+  Mutation: {
+    getRandomInt:(_, args) => parseInt(Math.random(), 10),
+  }
 }
 
 const server = new GraphQLServer({ typeDefs, resolvers })
